7cc425407ad6f69275acf61cc8ff55c3c0f0ca89
Miguel Halys Ortuño <mhalys91@gmail.com>
2018-04-06 18:34:52 +0000

    Update stormKafkaTopology.java
    feat: added KafkaBolt constructor with topic sink.

--- src/main/java/stormKafkaTopology.java
+++ src/main/java/stormKafkaTopology.java
@@ -13,17 +13,20 @@ public class stormKafkaTopology {
     private static final String KAFKA_SPOUT = "KafkaSpout";
     private static final String FILTER_BOLT = "filterBolt";
     private static final String KAFKA_WRITE_BOLT = "KafkaWriteBolt";
+    private static final String KAFKA_WRITE_ELSE_BOLT = "KafkaElseWriteBolt";
     private static final String TOPOLOGY_NAME = "MyTopologyKafkaStorm";
 
     public static void main(String[] args) throws Exception {
         int numSpoutExecutors = 1;
         KafkaSpout kspout = buildKafkaSentenceSpout();
         TextFilterBolt TFBolt = new TextFilterBolt();
-        KafkaBolt KfBolt = new KafkaBolt();
+        KafkaBolt KfBolt = new KafkaBolt("kafkatopic2");
+        KafkaBolt KfBoltElse = new KafkaBolt("kafkatopic3");
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout(KAFKA_SPOUT, kspout, numSpoutExecutors);
         builder.setBolt(FILTER_BOLT, TFBolt).shuffleGrouping(KAFKA_SPOUT);
-        builder.setBolt(KAFKA_WRITE_BOLT, KfBolt).shuffleGrouping(FILTER_BOLT);
+        builder.setBolt(KAFKA_WRITE_BOLT, KfBolt).shuffleGrouping(FILTER_BOLT,"stream1");
+        builder.setBolt(KAFKA_WRITE_ELSE_BOLT, KfBoltElse).shuffleGrouping(FILTER_BOLT,"stream2");
         Config conf = new Config();
         if (args != null && args.length > 0) {
             conf.setNumWorkers(1);
@@ -32,9 +35,9 @@ public class stormKafkaTopology {
         else {
             LocalCluster cluster = new LocalCluster();
             cluster.submitTopology(TOPOLOGY_NAME, conf, builder.createTopology());
-            Utils.sleep(10000);
-            cluster.killTopology(TOPOLOGY_NAME);
-            cluster.shutdown();
+//            Utils.sleep(10000);
+//            cluster.killTopology(TOPOLOGY_NAME);
+//            cluster.shutdown();
         }
     }
     private static KafkaSpout buildKafkaSentenceSpout() {
